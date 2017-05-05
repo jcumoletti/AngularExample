4de940ab4e52020efac6a4c903ede0b1a35b755d
Jeremy Cumoletti <jeremy@glyphmobile.com>
2017-05-05 02:06:32 -0400

    Fixed. Removed about section incompletely

--- src/app/app.js
+++ src/app/app.js
@@ -2,10 +2,7 @@ angular.module( 'ngBoilerplate', [
   'templates-app',
   'templates-common',
   'ngBoilerplate.home',
-  'ngBoilerplate.about',
   'ui.router'
-  
-
 ])
 
 .config( function myAppConfig ( $stateProvider, $urlRouterProvider ) {
